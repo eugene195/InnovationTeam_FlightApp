9cd50c5f887576e65698b641ef8c593f0d1c29ac
eugene195 <eugene.07@list.ru>
2015-04-29 09:28:07 +0200

    First working project copy

---
+++ FlightApp/app/src/main/java/com/flightapp/innovationflightapp/FlightList.java
@@ -0,0 +1,15 @@
+package com.flightapp.innovationflightapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eugene on 29.04.15.
+ */
+public class FlightList extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flightlist);
+    }
+}
---
+++ FlightApp/app/src/main/java/com/flightapp/innovationflightapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.flightapp.innovationflightapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private Button page1, page2, page3, page4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        page1 = (Button)findViewById(R.id.page1);
+        page1.setOnClickListener(this);
+        page2 = (Button) findViewById(R.id.page2);
+        page2.setOnClickListener(this);
+        page3 = (Button) findViewById(R.id.page3);
+        page3.setOnClickListener(this);
+        page4 = (Button) findViewById(R.id.page4);
+        page4.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        final Context context=this;
+        switch(v.getId())
+        {
+            case R.id.page1:
+                Intent i = new Intent(context, FlightList.class);
+                startActivity(i);
+                break;
+        }
+    }
+}
